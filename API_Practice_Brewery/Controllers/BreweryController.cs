9bdb6e51ae721ea03f98467fe3a7c014001625fa
jamesjr19 <ruwartja@gmail.com>
2020-04-15 13:36:01 -0400

    generate random brewery method and view

--- API_Practice_Brewery/Controllers/BreweryController.cs
+++ API_Practice_Brewery/Controllers/BreweryController.cs
@@ -39,5 +39,23 @@ namespace API_Practice_Brewery.Controllers
             return View(breweries);
         }
 
+        public async Task<IActionResult> RandomBrewery()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://brianiswu-open-brewery-db-v1.p.rapidapi.com");
+            client.DefaultRequestHeaders.Add("x-rapidapi-host", "brianiswu-open-brewery-db-v1.p.rapidapi.com");
+            client.DefaultRequestHeaders.Add("x-rapidapi-key", "0495956901msh0faebcb59561518p1e45ebjsnbd272e41ebb3");
+
+            //generate a random 4 digit id number
+            Random num = new Random();
+            int id = num.Next(0, 6000);
+            
+
+            //plug that id number into api call
+            var response = await client.GetAsync($"/breweries/{id}");
+            var brewery = await response.Content.ReadAsAsync<Brewery>();
+            
+            return View(brewery);
+        }
     }
 }
